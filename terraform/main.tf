86475d2a94f7d269ccf86a9e4d7967ceadafe12e
Alvin Tang <alvintang@gmail.com>
2021-09-05 16:20:49 +0800

    added other tf resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,9 +14,39 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "aws_key_pair" "my_key" {
+  key_name   = "my_key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "example" {
+  name = "example"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["112.201.99.175/32"]
+  }
+
+  # ALLOW ALL egress
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Env = var.env
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
+  key_name      = aws_key_pair.my_key.key_name
+  vpc_security_group_ids = [aws_security_group.example.id]
 
   tags = {
     Owner = "Github"
